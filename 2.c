87eeba7f37d696fa560b09b5554e89148f931f26
fujie <fujie.me@qq.com>
2020-03-26 20:25:50 +0800

    修改第二题

--- 2.c
+++ 2.c
@@ -6,11 +6,21 @@ int main(){
 	int r;
 	int pr = 0;
 	double s = 0;
+	if(n % 2 == 0){
+		
+	}
 	for(int i = 0; i<n; i++){
 		scanf("%d", &r);
-		if(i % 2 == 0){
-			s = s + PI * (r*r - pr*pr);
+		if(n % 2 == 1){
+		    if(i % 2 == 0){
+			    s = s + PI * (r*r - pr*pr);
+		    }
+		}else{
+			if(i % 2 == 1){
+			    s = s + PI * (r*r - pr*pr);
+		    }
 		}
+
 		pr = r;
 	}
 	printf("%0.5lf\n", s);
